cd52cce3cca4987b1a6bbc7970a8f96e78046f2b
FJW <903683902@qq.com>
2016-09-27 17:08:49 +0800

    修改Foundation_Categories

--- Categories/Foundation/Foundation_Categories.h
+++ Categories/Foundation/Foundation_Categories.h
@@ -9,5 +9,20 @@
 #ifndef Foundation_Categories_h
 #define Foundation_Categories_h
 
+#pragma mark - NSString 
+
+#import "NSString+MD5.h" // md5
+
+#import "NSString+CGSize.h" // 计算文本尺寸
+
+#import "NSString+RMB.h"  // 人民币格式
+
+#import "NSString+CreateDate.h" // 时间格式
+
+#pragma mark - NSObject
+
+#import "NSObject+SafeObject.h" // 安全取数组
+
+#import "NSObject+SwiizzleMethod.h" // 运行时替换方法
 
 #endif /* Foundation_Categories_h */
