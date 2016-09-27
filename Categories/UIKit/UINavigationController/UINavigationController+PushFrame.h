06528ba21820f7c7eb3d4d52a0d6962fea5f8ce8
cezres <cezr@sina.com>
2016-09-27 16:56:46 +0800

    UINavigationController 从指定索引切换到新的视图控制器

--- Categories/UIKit/UINavigationController/UINavigationController+PushFrame.h
+++ Categories/UIKit/UINavigationController/UINavigationController+PushFrame.h
@@ -10,13 +10,23 @@
 
 @interface UINavigationController (PushFrame)
 
+/// 当前控制器
 @property (assign, nonatomic, readonly) __kindof UIViewController *currentViewController;
 
+/// 第一个控制器
 @property (strong, nonatomic, readonly) __kindof UIViewController *firstViewController;
 
-@property (assign, nonatomic, readonly) __kindof UIViewController *lastViewController;
+/// 倒数第二个控制器
+@property (assign, nonatomic, readonly) __kindof UIViewController *previouViewController;
 
 
-
+/**
+ *  从指定索引切换到新的视图控制器
+ *
+ *  @param viewController <#viewController description#>
+ *  @param index          <#index description#>
+ *  @param animated       <#animated description#>
+ */
+- (void)pushViewController:(UIViewController *)viewController frameIndex:(NSInteger)index animated:(BOOL)animated;
 
 @end
--- Categories/UIKit/UINavigationController/UINavigationController+PushFrame.m
+++ Categories/UIKit/UINavigationController/UINavigationController+PushFrame.m
@@ -7,20 +7,46 @@
 //
 
 #import "UINavigationController+PushFrame.h"
+#import "UIView+Capture.h"
 
 @implementation UINavigationController (PushFrame)
 
 
 - (void)pushViewController:(UIViewController *)viewController frameIndex:(NSInteger)index animated:(BOOL)animated {
-    if (index + 1 > self.viewControllers.count) {
+    if (index + 1 > self.viewControllers.count) {   // 索引错误
         return;
     }
-    else if (index + 1 == self.viewControllers.count) {
+    else if (index + 1 == self.viewControllers.count) { // 如果索引是当前控制器，直接Push
         [self pushViewController:viewController animated:animated];
         return;
     }
     
+    UIViewController *currentViewController = self.currentViewController;
+    UIViewController *frameViewController = self.viewControllers[index];
     
+    
+    [self popToViewController:frameViewController animated:NO];
+    
+    if (animated) {
+        // 为目标视图控制器添加截屏视图
+        UIImageView *captureView = [[UIImageView alloc] init];
+        captureView.image = currentViewController.view.capture;
+        captureView.frame = [UIScreen mainScreen].bounds;
+        [frameViewController.view addSubview:captureView];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self pushViewController:viewController animated:YES];
+        });
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [captureView removeFromSuperview];
+        });
+        
+    }
+    else {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self pushViewController:viewController animated:NO];
+        });
+    }
     
 }
 
@@ -29,7 +55,7 @@
     return self.viewControllers[self.viewControllers.count];
 }
 
-- (UIViewController *)lastViewController {
+- (UIViewController *)previouViewController {
     if (self.viewControllers.count < 2) {
         return NULL;
     }
