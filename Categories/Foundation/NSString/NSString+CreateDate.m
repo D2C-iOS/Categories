f9b4ff3f49c1a06f4a7171d4dc8e816eb24d1281
FJW <903683902@qq.com>
2016-09-27 17:17:44 +0800

    添加时间格式

---
+++ Categories/Foundation/NSString/NSString+CreateDate.m
@@ -0,0 +1,28 @@
+//
+//  NSString+CreateDate.m
+//  Categories
+//
+//  Created by fengjiwen on 16/9/27.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import "NSString+CreateDate.h"
+
+@implementation NSString (CreateDate)
+
+- (NSString *)interval {
+    return self;
+}
+
+- (NSString *)intdata {
+    return self;
+}
+
+- (NSString *)intCdate {
+    return self;
+}
+
+- (NSString *)intPAdate {
+    return self;
+}
+@end
